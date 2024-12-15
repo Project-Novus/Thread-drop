40bef82e290dc397456b13cf539cbff21c2d69b3
Mohammed Faizan Shariff <71890836+mrfaizanshariff@users.noreply.github.com>
2024-12-15 18:16:29 +0530

    home page changes to make website live

--- thread-drop-clothing-ui/src/app/app.component.ts
+++ thread-drop-clothing-ui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { ShopifyService } from './core/services/shopify.service';
 
@@ -10,6 +10,8 @@ import { ShopifyService } from './core/services/shopify.service';
 export class AppComponent implements OnInit {
   title = 'THREAD DROP';
   isSplashVisible = true;
+  @ViewChild('backgroundVideo') backgroundVideo!: ElementRef<HTMLVideoElement>;
+  userInteracted: boolean =false;
   constructor(private router:Router, private shopifyService:ShopifyService){}
   ngOnInit(): void {
     // this.createCustomer()
@@ -74,4 +76,47 @@ export class AppComponent implements OnInit {
       
     })
   }
+
+  // @ViewChild('videoPlayer') videoPlayer!: ElementRef;
+   @ViewChild('textElement') textElement!: ElementRef; 
+  //  ngAfterViewInit() { this.videoPlayer.nativeElement.play(); }
+   showText() { this.textElement.nativeElement.style.display = 'block'; }
+
+
+
+
+   onVideoLoaded(event: Event): void {
+    const video = this.backgroundVideo.nativeElement;
+    
+    
+    // If the user has interacted with the page, play the video
+    if (this.userInteracted) {
+      video.play().catch((err: any) => console.log('Play failed:', err));
+     
+    }
+  }
+
+  // Monitor video playback to loop between specific times
+  
+
+  // Listen for user interaction to trigger the video play
+  @HostListener('window:click') onUserInteraction() {
+    this.playVideo();
+  }
+  @HostListener('window:scroll') onUserScroll() {
+    this.playVideo();
+  }
+
+  // Play video only after user interaction
+  playVideo(): void {
+    const video = this.backgroundVideo.nativeElement;
+    this.userInteracted = true; // User has interacted with the page
+    this.textElement.nativeElement.style.display = 'none';
+    // Attempt to play the video once user interaction is detected
+    if (video.paused) {
+      video.play().catch((err: any) => console.log('Play failed:', err));
+    }
+  }
 }
+
+
